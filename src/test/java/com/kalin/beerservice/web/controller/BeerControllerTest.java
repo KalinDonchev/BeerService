42849569a2403277fdbb98c3829100774eb6763e
KalinDonchev <kalindonchev@mail.bg>
2021-11-11 16:18:23 +0200

    refactor pom

--- src/test/java/com/kalin/beerservice/web/controller/BeerControllerTest.java
+++ src/test/java/com/kalin/beerservice/web/controller/BeerControllerTest.java
@@ -1,6 +1,5 @@
 package com.kalin.beerservice.web.controller;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.kalin.beerservice.web.model.BeerDto;
 import org.junit.jupiter.api.Test;
